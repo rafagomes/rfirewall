9576f791516f1be17ea901b8fd9ed329e132c2cb
rafagomes <rafaelgomes.web@gmail.com>
2024-09-13 18:23:43 -0300

    Refactor Rule struct to use cloned values instead of Option types

--- rfirewall/src/core/rule.rs
+++ rfirewall/src/core/rule.rs
@@ -2,12 +2,12 @@ use crate::infrastructure::rule;
 use pnet::packet::ethernet::EthernetPacket;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Rule {
     pub action: String, // allow or deny
-    pub ip: Option<String>,
-    pub port: Option<u16>,
-    pub protocol: Option<String>,
+    pub ip: String,
+    pub port: u16,
+    pub protocol: String,
 }
 
 impl Rule {
@@ -17,36 +17,36 @@ impl Rule {
         println!("Packet: {:?}", packet);
 
         // Example logic, replace with actual matching logic:
-        if let Some(ref _ip) = self.ip {
-            // Check IP
-        }
-        if let Some(_port) = self.port {
-            // Check port
-        }
-        if let Some(ref _protocol) = self.protocol {
-            // Check protocol
-        }
+        // if let ref _ip = self.ip {
+        //     // Check IP
+        // }
+        // if let _port = self.port {
+        //     // Check port
+        // }
+        // if let ref _protocol = self.protocol {
+        //     // Check protocol
+        // }
 
         true // Placeholder
     }
 
-    pub fn add_rule(allow: bool, deny: bool, port: u16, ip: &String, protocol: &String) {
+    pub fn new(allow: bool, port: u16, ip: &str, protocol: &str) -> Self {
         let action = if allow {
             "allow".to_string()
-        } else if deny {
-            "deny".to_string()
         } else {
-            panic!("Either allow or deny must be true");
+            "deny".to_string()
         };
 
         let new_rule = Self {
             action,
-            ip: Some(ip.to_string()),
-            port: Some(port),
-            protocol: Some(protocol.to_string()),
+            ip: ip.to_string(),
+            port: port,
+            protocol: protocol.to_string(),
         };
 
-        rule::save(new_rule);
+        rule::save(&new_rule);
+
+        new_rule
     }
 
     pub fn get_rules() -> Vec<Self> {
